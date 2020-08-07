14955efcaaf616e56a29458993ded6ea3389ed1a
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-08-07 21:07:40 +0800

    增加命名空间

--- data_structure/linked_list/index.ts
+++ data_structure/linked_list/index.ts
@@ -1,51 +1,53 @@
-interface LinkedList {
-  val: String;
-  next: LinkedList;
-}
-const d: LinkedList = {
-  val: "d",
-  next: null
-};
-const c: LinkedList = {
-  val: "c",
-  next: d
-};
-const b: LinkedList = {
-  val: "b",
-  next: c
-};
-const a: LinkedList = {
-  val: "a",
-  next: b
-};
+namespace index {
+  interface LinkedList {
+    val: String;
+    next: LinkedList;
+  }
+  const d: LinkedList = {
+    val: "d",
+    next: null
+  };
+  const c: LinkedList = {
+    val: "c",
+    next: d
+  };
+  const b: LinkedList = {
+    val: "b",
+    next: c
+  };
+  const a: LinkedList = {
+    val: "a",
+    next: b
+  };
 
-//遍历链表
-let p = a;
-while (p) {
-  console.log(p.val);
-  p = p.next;
-}
+  //遍历链表
+  let p = a;
+  while (p) {
+    console.log(p.val);
+    p = p.next;
+  }
 
-console.log("c和d中间插入e");
-//c和d中间插入e
-let e: LinkedList = {
-  val: "e",
-  next: d
-};
-c.next = e;
+  console.log("c和d中间插入e");
+  //c和d中间插入e
+  let e: LinkedList = {
+    val: "e",
+    next: d
+  };
+  c.next = e;
 
-//遍历链表
-let point = a;
-while (point) {
-  console.log(point.val);
-  point = point.next;
-}
+  //遍历链表
+  let point = a;
+  while (point) {
+    console.log(point.val);
+    point = point.next;
+  }
 
-console.log("删除e");
-c.next = d;
-//遍历链表
-let point_d = a;
-while (point_d) {
-  console.log(point_d.val);
-  point_d = point_d.next;
+  console.log("删除e");
+  c.next = d;
+  //遍历链表
+  let point_d = a;
+  while (point_d) {
+    console.log(point_d.val);
+    point_d = point_d.next;
+  }
 }
