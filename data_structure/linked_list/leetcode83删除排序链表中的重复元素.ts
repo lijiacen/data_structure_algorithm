dfdddab4f107012c5ee2b882c21f489af469c9c1
lijiacen <50915968+lijiacen@users.noreply.github.com>
2020-08-08 16:13:27 +0800

    leetcode83删除排序链表中的重复元素.ts

---
+++ data_structure/linked_list/leetcode83删除排序链表中的重复元素.ts
@@ -0,0 +1,65 @@
+namespace leetcode83 {
+  class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.next = next === undefined ? null : next;
+    }
+  }
+
+  function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let p = head;
+    let res = null;
+    let val = -1;
+    if (p) {
+      val = p.val;
+      res = new ListNode(p.val);
+      let r = res;
+
+      while (p) {
+        if (val !== p.val) {
+          r.next = new ListNode(p.val);
+          r = r.next;
+        }
+        val = p.val;
+        p = p.next;
+      }
+    }
+
+    return res;
+  }
+
+  const g: ListNode = {
+    val: 5,
+    next: null
+  };
+  const f: ListNode = {
+    val: 4,
+    next: g
+  };
+  const e: ListNode = {
+    val: 3,
+    next: f
+  };
+  const d: ListNode = {
+    val: 2,
+    next: e
+  };
+  const c: ListNode = {
+    val: 2,
+    next: d
+  };
+
+  const b: ListNode = {
+    val: 1,
+    next: c
+  };
+  const a: ListNode = {
+    val: 1,
+    next: b
+  };
+  //const a1: ListNode = null;
+  // console.log(deleteDuplicates(a1));
+  console.log(deleteDuplicates(a));
+}
