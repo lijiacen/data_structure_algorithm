437324a160a3a52258213553797a7155ab13fb30
lijiacen <lijiacen880915@163.com>
2020-08-17 15:57:22 +0800

    修复bug

--- data_structure/set/index.ts
+++ data_structure/set/index.ts
@@ -1,6 +1,6 @@
 //去重
-const arr: Array<Number> = [1, 1, 2, 3, 3, 4];
-const set: Set<Number> = new Set(arr);
+const arr: number[] = [1, 1, 2, 3, 3, 4];
+const set: Set<number> = new Set(arr);
 // console.log(set);
 
 //判断元素是否在集合中
@@ -10,10 +10,10 @@ const has6: boolean = set.has(6);
 // console.log(has6);
 
 //求元素交集
-const arr2: Array<Number> = [3, 3, 4, 4, 5, 6];
-const set2: Set<Number> = new Set(arr2);
-const arr3: Array<Number> = [];
-const set3: Set<Number> = new Set(arr3);
+const arr2: number[] = [3, 3, 4, 4, 5, 6];
+const set2: Set<number> = new Set(arr2);
+const arr3: number[] = [];
+const set3: Set<number> = new Set(arr3);
 
 set.forEach((i) => {
   set2.has(i) && set3.add(i);
